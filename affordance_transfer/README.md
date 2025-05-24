5114f9bd0a605647c04ccf4dfe658249ca0c6df5
Joji <jojijoseph@iisc.ac.in>
2025-05-24 11:37:19 +0530

    Add scripts for evaluating affordance transfer

---
+++ affordance_transfer/README.md
@@ -0,0 +1,12 @@
+# README
+
+The code need to be refactored and not documented.
+
+To run,
+
+1. Move these scripts one folder up
+2. Download the affordance annotation and dataset from https://github.com/JojiJoseph/3dgs-gradient-segmentation
+3. Back-project dino features
+4. Run `sh affordance.sh`
+
+Please do raise an issue if something is not working.
---
+++ affordance_transfer/affordance.sh
@@ -0,0 +1,3 @@
+python demo_affordance_transfer.py --data-dir data/processed_scene_01/ --checkpoint data/processed_scene_01/ckpts/ckpt_29999_rank0.pt --results-dir results/ps01 --rasterizer gsplat
+python demo_affordance_transfer.py --data-dir data/processed_scene_02/ --checkpoint data/processed_scene_02/ckpts/ckpt_29999_rank0.pt --results-dir results/ps02 --rasterizer gsplat
+python demo_affordance_transfer.py --data-dir data/processed_scene_03/ --checkpoint data/processed_scene_03/ckpts/ckpt_29999_rank0.pt --results-dir results/ps03 --rasterizer gsplat
